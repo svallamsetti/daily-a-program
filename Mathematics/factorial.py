ce47c39926ca1a32f8b894180b818b78f77ec1b9
saivallamsetti <saivallamsetti@email.arizona.edu>
2020-08-13 14:01:59 -0700

    Factorial using recurssion

---
+++ Mathematics/factorial.py
@@ -0,0 +1,17 @@
+def factorial(n):
+        if(n <= 1):
+            return 1
+        else: 
+            return n * factorial(n-1)
+
+print(factorial(0))
+print(factorial(1))
+print(factorial(2))
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
+print(factorial(8))
+print(factorial(9))
+print(factorial(10))
