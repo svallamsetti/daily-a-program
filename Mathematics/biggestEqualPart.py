a97d9fbd676b0f31243c9d2cbb3335b419068cca
Sai Dinesh <vallamsettisaidinesh@gmail.com>
2020-12-21 11:12:53 -0700

    Update biggestEqualPart.py

--- Mathematics/biggestEqualPart.py
+++ Mathematics/biggestEqualPart.py
@@ -1,18 +1,17 @@
 def biggestEqualPart(x, y):
     if( x == y ):
-        print(x, y)
-        return
+        return (x, y)
     else:
         if(x>y):
             if(x>y*2):
-                biggestEqualPart(x-(y*2), y)
+                return biggestEqualPart(x-(y*2), y)
             else:
-                biggestEqualPart(x-y, y)
+                return biggestEqualPart(x-y, y)
         else:
             if(y>x*2):
-                biggestEqualPart(x, y-(x*2))
+                return biggestEqualPart(x, y-(x*2))
             else:
-                biggestEqualPart(x, y-x)
+                return biggestEqualPart(x, y-x)
 
 biggestEqualPart(5,4)
 biggestEqualPart(1680,640)
